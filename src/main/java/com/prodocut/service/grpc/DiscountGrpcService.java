b94d93d63f2eb12b1bc21a49b70302c74a175f42
ozlemozk <58657181+ozlemozk@users.noreply.github.com>
2024-01-24 21:28:25 +0300

    grpc

--- src/main/java/com/prodocut/service/grpc/DiscountGrpcService.java
+++ src/main/java/com/prodocut/service/grpc/DiscountGrpcService.java
@@ -1,4 +1,9 @@
 package com.prodocut.service.grpc;
 
+import com.prodocut.grpc.DiscountRequest;
+import com.prodocut.grpc.DiscountResponse;
+
 public interface DiscountGrpcService {
+    DiscountResponse getDiscount(DiscountRequest discountRequest); //proto ile aynı olmak zorunda olamayabilir
+
 }
--- src/main/java/com/prodocut/service/grpc/impl/DiscountGrpcServiceImpl.java
+++ src/main/java/com/prodocut/service/grpc/impl/DiscountGrpcServiceImpl.java
@@ -1,4 +1,41 @@
 package com.prodocut.service.grpc.impl;
 
-public class DiscountGrpcServiceImpl {
+import com.prodocut.grpc.DiscountServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import com.prodocut.grpc.DiscountRequest;
+import com.prodocut.grpc.DiscountResponse;
+import com.prodocut.service.grpc.DiscountGrpcService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+//@GrpcService  //bunu yapınca otomatik kanal vs oluştuyır
+@Service
+
+public class DiscountGrpcServiceImpl implements DiscountGrpcService {
+
+
+    private DiscountServiceGrpc.DiscountServiceBlockingStub discountServiceBlockingStub;
+    //tamamlanıncaya kadar bekler bloklar tamlanıncaya kadar karışı tarafa gönderilen istek için
+    private ManagedChannel channel; //karşı servisin portu ve host yazılır
+
+
+    //@Value("${discount.grpc.host} burada applic yaml dan gelen değeri alıcak grpc host ve port belirlenir
+    public DiscountGrpcServiceImpl(@Value("localhost") String grpcHost, @Value("9090") int grpcPort) {
+        System.out.println("--> Discount grpc: " + grpcHost + " " + grpcPort);
+        channel = ManagedChannelBuilder.forAddress(grpcHost, grpcPort)
+                .usePlaintext() //
+                .build();
+        //usePlaintext():Bu, geliştirme veya test sırasında, güvenliği gerektirmeyen durumlar için kullanışlı olabilir.
+        // /*useTransportSecurity*/
+    }
+
+    @Override
+    public DiscountResponse getDiscount(DiscountRequest discountRequest) {
+        discountServiceBlockingStub = DiscountServiceGrpc.newBlockingStub(channel);//respone donunye kadar bekeldiğim yer
+        DiscountResponse discountResponse = discountServiceBlockingStub.getDiscount(discountRequest);
+        return discountResponse;
+    }
 }
